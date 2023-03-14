00bb0f69e768f11bbeb795c866b39d2380f8db64
T21MCA047 <100526431+rachakondanarendra@users.noreply.github.com>
2023-03-14 12:50:18 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number:");
+        a= sc.nextInt();
+        System.out.println("Enter second number:");
+        b= sc.nextInt();
+        System.out.println("Addition:"+(a+b));
+
+
+    }
+}
